6c7954193ce042e64a8d54bd866d931ee9e2f797
shortstuff8263 <meganleeharrington@gmail.com>
2026-09-20 16:41:55 -0400

    Delete 587-Find-Customer-Referee.sql

--- 587-Find-Customer-Referee.sql
+++
@@ -1,7 +0,0 @@
---584. Find Customer Referee
-
-select name
-from customer
-where referee_id != 2 OR referee_id IS NULL
-
---https://leetcode.com/problems/find-customer-referee/submissions/1911614756
